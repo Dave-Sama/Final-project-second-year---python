d8245a0edeb9d48fb1737e912c8e2eff17c49015
david <davech1228@gmail.com>
2019-12-22 21:06:33 +0200

    added the Accept button for the Student bar, a few commits throughout the code too.

--- data_base.py
+++ data_base.py
@@ -1,8 +1,14 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import tkinter as tk
+
+
+
 # user_data represent the login and password of the users in the system
 users_data = {'a': ['a', 'S'], 'b': ['b', 'L'], 'c': ['c', 'C']}
 
 # questions represent the questions that are stored in the database of the system
 # each question are stored into a dictionary with the following format:
-# {key:" specific ID of the question " : values: "list of:"[ 'Question itself', 'Course' 'Sub-subject',
-# , 'Difficulty', 'Year', 'Has an answer', 'From exam/quiz', 'Semester', 'Period', 'Format']}
-questions = {}
+# {key:" specific ID of the question " : values: "list of:"[ 'Question itself', 'Course', 'Sub-subject',
+# , 'Difficulty', 'Year', 'Has an answer', 'From exam/quiz', 'Semester', 'Format']}
+questions = {1: ['hello', 'Calculus1', 'Integrals', 'Easy', 2019, 'Yes', 'Exam', 'A', 'Docx'], 2: ['moto', 'Calculus2', 'banana', 'hard', 2020, 'No', 'Quiz', 'A', 'Pdf']}
--- login.py
+++ login.py
@@ -2,13 +2,14 @@
 # all the imported libraries/API for this project
 from tkinter import *
 from tkinter import messagebox
+from PIL import ImageTk,Image
 from tkinter.ttk import *
 from data_base import *
 
 class GUI(object):
     def __init__(self):
         # setting the root
-        root = self.root = Tk()
+        root = self.root = tk.Tk()
         root.title('Test')
         root.geometry('310x85')
 
@@ -55,73 +56,203 @@ class GUI(object):
                     9. format of the question
                     """
             # creating the defining the new layer.
-            self.student_top.geometry('825x400')
+            self.student_top.geometry('840x430')
 
             # creating a Label for courses:
             Label(self.student_top, text='Course: ').grid(row=0, column=0)
 
             # creating combobox for the courses:
-            combo_course = Combobox(self.student_top)
-            combo_course['values'] = ('Calculus1', 'Linear algebra', 'Pre computer science', 'Architecture', "Logic 1")
-            combo_course.current(0)  # set the selected item
-            combo_course.grid(row=0, column=1, sticky='W')
+            self.combo_course = Combobox(self.student_top)
+            self.combo_course['values'] = ('Calculus1', 'Linear algebra', 'Pre computer science', 'Architecture', "Logic 1")
+            self.combo_course.current(0)  # set the selected item
+            self.combo_course.grid(row=0, column=1, sticky='W')
+
+
 
             # creating a Label for courses:
             Label(self.student_top, text='   Sub subject: ').grid(row=0, column=3)
 
             # creating combobox for the sub-subjects:
-            combo_sub_subject = Combobox(self.student_top)
-            combo_sub_subject['values'] = ('not yet', 'not yet', 'not yet', 'not yet', "not yet")
-            combo_sub_subject.current(0)  # set the selected item
-            combo_sub_subject.grid(row=0, column=4)
+            self.combo_sub_subject = Combobox(self.student_top)
+            self.combo_sub_subject['values'] = ('Integrals', 'not yet', 'not yet', 'not yet', "not yet")
+            self.combo_sub_subject.current(0)  # set the selected item
+            self.combo_sub_subject.grid(row=0, column=4)
 
             # creating a Label for Difficulty:
             Label(self.student_top, text='  Difficulty: ').grid(row=0, column=6)
 
             # creating combobox for the difficulty:
-            combo_difficulty = Combobox(self.student_top)
-            combo_difficulty['values'] = ('Easy', 'Moderate', 'Hard')
-            combo_difficulty.current(0)  # set the selected item
-            combo_difficulty.grid(row=0, column=7, sticky='W')
+            self.combo_difficulty = Combobox(self.student_top)
+            self.combo_difficulty['values'] = ('Easy', 'Moderate', 'Hard')
+            self.combo_difficulty.current(0)  # set the selected item
+            self.combo_difficulty.grid(row=0, column=7, sticky='W')
 
             # creating a Label for Answers:
             Label(self.student_top, text='  Answers: ').grid(row=0, column=9)
 
             # creating combobox for the Answers:
-            combo_answers = Combobox(self.student_top)
-            combo_answers['values'] = ('Yes', 'No')
-            combo_answers.current(0)  # set the selected item
-            combo_answers.grid(row=0, column=10, sticky='W')
+            self.combo_answers = Combobox(self.student_top)
+            self.combo_answers['values'] = ('Yes', 'No')
+            self.combo_answers.current(0)  # set the selected item
+            self.combo_answers.grid(row=0, column=10, sticky='W')
 
             # creating a Label for Years:
-            Label(self.student_top, text='Year:').grid(row=1, column=0)
+            Label(self.student_top, text='Year:').place(x=1, y=22)
 
             # creating spinbox for the Years:
             var = IntVar()
             var.set(2019)
-            spin_years = Spinbox(self.student_top, from_=1995, to=2020, width=6, textvariable=var)
-            spin_years.place(x=47, y=22, width=143)
+            self.spin_years = Spinbox(self.student_top, from_=1995, to=2020, width=6, textvariable=var)
+            self.spin_years.place(x=49, y=22, width=144)
 
             # creating Label for the semester:
-            Label(self.student_top, text='Semester:').place(x=199, y=22)
+            Label(self.student_top, text='Semester:').place(x=201, y=22)
 
             # creating a combobox for the Semester:
-            combo_semester = Combobox(self.student_top)
-            combo_semester['values'] = ('A', 'B', 'Summer')
-            combo_semester.current(0)  # set the selected item
-            combo_semester.place(x=270, y=22)
+            self.combo_semester = Combobox(self.student_top)
+            self.combo_semester['values'] = ('A', 'B', 'Summer')
+            self.combo_semester.current(0)  # set the selected item
+            self.combo_semester.place(x=274, y=22)
 
             # creating a Label for Format:
-            Label(self.student_top, text='Format:').place(x=419, y=22)
+            Label(self.student_top, text='Format:').place(x=423, y=22)
 
             # creating a combobox for Format:
-            combo_format = Combobox(self.student_top)
-            combo_format['values'] = ('docx', 'pdf', 'jpeg')
-            combo_format.current(0)  # set the selected item
-            combo_format.place(x=477, y=22)
+            self.combo_format = Combobox(self.student_top)
+            self.combo_format['values'] = ('Docx', 'Pdf', 'Jpeg')
+            self.combo_format.current(0)  # set the selected item
+            self.combo_format.place(x=483, y=22)
+
+            # creating Label for the Exam/Quiz:
+            Label(self.student_top, text='From:').place(x=632, y=22)
+
+            # creating a combobox for the Exam/Quiz:
+            self.combo_from = Combobox(self.student_top)
+            self.combo_from['values'] = ('Exam', 'Quiz')
+            self.combo_from.current(0)  # set the selected item
+            self.combo_from.place(x=688, y=22)
 
             # crating an Accept button:
-            Button(self.student_top, text='Accept', command=self.login_verify, width = 20).place(x=679, y=22)
+            Button(self.student_top, text='Accept', command=self.question_match, width=20).place(x=334, y=50)
+
+            # creating forward/backward button for image display:
+            self.button_forward = Button(self.student_top, text='>>', width=5, command=lambda: self.forward(2)).place(x=488, y=50)
+            self.button_back = Button(self.student_top, text='<<', width=5, command=self.back, state=DISABLED).place(x=285, y=50)
+
+
+    def forward(self, image_number):
+        """
+        this method is handling the logic behind the forward button
+        """
+
+
+        self.my_label.place_forget()
+        self.my_label = Label(self.student_top, image=self.questions_image[image_number-1])
+        self.my_label.image = self.questions_image[image_number-1]
+
+        self.button_forward = Button(self.student_top, text='>>', command=lambda: self.forward(image_number+1))
+        self.button_back = Button(self.student_top, text='<<', command=lambda: self.back(image_number-1))
+
+
+        #
+        # if image_number == len(self.questions_image):
+        #     self.button_forward = Button(self.student_top, text='>>', state=DISABLED)
+        #
+        self.my_label.place(x=260, y=100)
+        # self.button_forward = Button.place(x=488, y=50)
+        # self.button_back = Button.place(x=285, y=50)
+
+    def back(self, image_number):
+        """
+        this method handling the logic behind the back button
+        """
+
+
+        self.my_label.place_forget()
+        self.my_label = Label(image=self.questions_image[image_number - 1])
+        self.button_forward = Button(self.student_top, text='>>', command=lambda: self.forward(image_number + 1))
+        self.button_back = Button(self.student_top, text='<<', command=lambda: self.back(image_number - 1))
+
+        if image_number == 1:
+            button_back = Button(self.student_top,text='<<', state=DISABLED)
+
+        self.my_label.place(x=260, y=100)
+        self.button_forward = Button.place(x=488, y=50)
+        self.button_back = Button.place(x=285, y=50)
+
+    def question_match(self):
+        """
+        this method can find a match from the search bar, and return the matches to the bar
+        """
+
+        # temp1 for the data base , temp2 for the users input
+
+        # questions image is the most important list, here
+        self.questions_image = []
+        questions_from_base = []
+        temp1 = []
+        flag = False
+        i = 0
+
+        # we copied the values from 1 to 8 from each key in the dictionary:
+        for k in questions:
+            temp1.append(questions[k][1:])
+            questions_from_base.append((questions[k][0:1]))
+
+            # we need to convert the year to a string to compare it later
+            temp1[i][3] = str(temp1[i][3])
+
+        # putting all the user inputs into temp2
+        temp2 = [self.combo_course.get(), self.combo_sub_subject.get(), self.combo_difficulty.get(), self.spin_years.get(), self.combo_answers.get(), self.combo_from.get(), self.combo_semester.get(), self.combo_format.get()]
+
+        # checking if there is a match between temp1, and temp2
+        for i in range(0, len(temp1)):
+            if temp1[i] == temp2:
+                """ still str here, need to be changed later """
+                path = "happy_mango.jpeg"
+                img = ImageTk.PhotoImage(Image.open(path))
+                self.questions_image.append(img)
+                flag = True
+
+        # if the length is bigger than 0, it means there is a match - go into the image_display
+        if flag == True:
+            self.image_display()
+
+        elif flag == False:
+            self.error_message()
+
+
+    def error_message(self):
+        """
+        this method is giving an alet if there is no match between the user's input and database.
+        """
+        error_window = Toplevel(self.student_top)
+        Label(error_window, text='Cannot find a match').pack()
+
+    def image_display(self):
+        path = "banana.jpeg"
+        img = ImageTk.PhotoImage(Image.open(path))
+        path1 = "leopard.jpeg"
+        img1 = ImageTk.PhotoImage(Image.open(path1))
+
+        path2 = "panda.jpeg"
+        img2 = ImageTk.PhotoImage(Image.open(path2))
+
+        path3 = "pug.jpeg"
+        img3 = ImageTk.PhotoImage(Image.open(path3))
+
+        self.questions_image.append(img)
+        self.questions_image.append(img1)
+        self.questions_image.append(img2)
+        self.questions_image.append(img3)
+
+
+
+        # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+        self.my_label = tk.Label(self.student_top, image=self.questions_image[0])
+        self.my_label.image = self.questions_image[0]
+        # The Pack geometry manager packs widgets in rows or columns.
+        self.my_label.place(x=260, y=100)
 
     def create(self, key):
         """
@@ -154,6 +285,7 @@ class GUI(object):
         if there is no match - the program will present an alert to the user, otherwise, it will open the appropriate
         page.
         """
+
         k = users_data.keys() # verification of username in dictionary
         for names in k:
             if names == self.username_field.get():
