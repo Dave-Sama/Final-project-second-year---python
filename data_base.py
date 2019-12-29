77b074198b8ab4e06a48125a6b2a4903efc6c9d2
Dave-Sama <55929174+Dave-Sama@users.noreply.github.com>
2019-12-29 03:12:07 +0200

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -1,10 +1,10 @@
+import dill
 
 
-
-
+dill.load_session('dill.pkl')
 # user_data represent the login and password of the users in the system
 users_data = {'a': ['a', 'S'], 'b': ['b', 'L'], 'c': ['c', 'C']}
-
+dill.dump_session('db.pkl')
 # questions represent the questions that are stored in the database of the system
 # each question are stored into a dictionary with the following format:
 # {key:" specific ID of the question " : values: "list of:"[ 'Question itself', 'Course', 'Sub-subject',
